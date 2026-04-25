1721c33d930bb2d35f50895bc3dadd3cbd94acbc
A.K.Azad <kawshikislam53@gmail.com>
2026-04-25 23:08:25 +0600

    dropstudent

---
+++ dropstudent.py
@@ -0,0 +1,32 @@
+class Student:
+    student_list = []
+
+    def __init__(self, student_id, name, department, is_enrolled):
+        self.student_id = student_id
+        self.name = name
+        self.department = department
+        self.is_enrolled = is_enrolled
+
+        Student.add_student(self)
+
+    @classmethod
+    def add_student(cls, student_obj):
+        cls.student_list.append(student_obj)
+
+    def enroll_student(self):
+        if self.is_enrolled:
+            print(f"{self.name} is already enrolled.")
+        else:
+            self.is_enrolled = True
+            print(f"{self.name} has been enrolled successfully.")
+
+    def drop_student(self):
+        if not self.is_enrolled:
+            print(f"{self.name} is already not enrolled.")
+        else:
+            self.is_enrolled = False
+            print(f"{self.name} has dropped out successfully.")
+
+s1 = Student(101, "Tanzin Akter", "CSE", True)
+
+s1.drop_student()
